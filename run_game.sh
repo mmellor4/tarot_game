ca15c3a8453ab70b6cfa90f6a48f41373f65da7b
marie mellor <54967465+mmellor4@users.noreply.github.com>
2025-06-26 19:42:23 -0400

    Update run_game.sh

--- run_game.sh
+++ run_game.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Optional: Set up a virtual environment (if you want to use one)
+# python3 -m venv venv
+# source venv/bin/activate
+
+# install dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Run the game
 python3 code/main.py
